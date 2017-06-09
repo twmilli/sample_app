03ae9c9c011f2f81b2c56156b8da229de64ee2f0
Milliman <code@getbraintree.com>
2017-06-09 11:11:54 -0700

    finishing user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   get 'users/new'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   root 'static_pages#home'
 end
